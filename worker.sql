0036c3a93faf853ae0f8f4739baf591df5127bdd
Sergey Pushkin <pushkinsv@gmail.com>
2018-12-06 16:26:20 +0700

    better file reading function

--- worker.sql
+++ worker.sql
@@ -51,14 +51,14 @@ as $_$
 	update fs.files set container=_container where id=_item returning container;
 $_$;
 
-create or replace function fs.file(_id int8)
-returns table (path int8[], id int8, md5 char(32), mime_type text, name text, size int, modified timestamp with time zone, data text, is_container boolean, expires timestamp with time zone) language plpgsql as $_$
+create or replace function fs.file(_id int8,_depth int default null,_floor int default 0)
+returns table (path int8[], id int8, md5 char(32), mime_type text, name text, size int, modified timestamp with time zone, data text, is_container boolean, expires timestamp with time zone, level int) language plpgsql as $_$
 declare
 	path int8[];
 begin
 	with
 	recursive r as (
-		select array[f.id]::int8[] as p,false as c, * from fs.files f where f.id=_id
+		select array[f.id]::int8[] as p,false as c, * from (select x as id,f.md5,f.mime_type,f.name,f.size,f.modified,f.container,f.is_container,f.expires from (values(_id)) as x(x) left join fs.files f on f.id=x) f
 		union
 		select f.id||p,f.id=any(p),f.* from  fs.files f
 		join r on r.container=f.id
@@ -69,14 +69,17 @@ begin
 
 	return query
 	with recursive r as (
-		select array[]::int8[] as o, array[f.id]::int8[] as p, false as c,* from fs.files f where f.id=_id
+		select array[]::int8[] as o, array[f.id]::int8[] as p, false as c,* from (select x as id,f.md5,f.mime_type,f.name,f.size,f.modified,f.container,coalesce(f.is_container,true) as is_container,f.expires from (values(_id)) as x(x) left join fs.files f on f.id=x) f
 		union
 		select r.o||row_number() over (partition by r.p order by f.name,f.id), p||f.id,f.id=any(p),f.* from fs.files f 
 		join r on r.id=f.container
 		where not c
 	)
-	select path||p,r.id,r.md5,r.mime_type,r.name,r.size,r.modified,fs.data_path()||'/'||substr(r.md5,1,3)||'/'||r.md5,r.is_container,r.expires from r
+	select path||p,r.id,r.md5,r.mime_type,r.name,r.size,r.modified,fs.data_path()||'/'||substr(r.md5,1,3)||'/'||r.md5,r.is_container,r.expires,cardinality(p)-_floor-1 from r
+	where cardinality(p)-1>=_floor
+	and cardinality(p)-_floor-2<coalesce(_depth,cardinality(p))
 	order by o
 	;
 end
 $_$;
+		select array[f.id]::int8[] as p,false as c, * from (select x as id,f.md5,f.mime_type,f.name,f.size,f.modified,f.container,f.is_container,f.expires from (values(11)) as x(x) left join fs.files f on f.id=x) f
